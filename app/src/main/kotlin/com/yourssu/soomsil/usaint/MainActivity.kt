ba6ae58359bf5f1ff863098d152069c1b5d89248
kangyuri1114 <yuri3334@gmail.com>
2024-12-16 17:55:03 +0900

    add : NavHost 생성 및 세팅

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.yourssu.design.system.compose.base.YdsScaffold
+import com.yourssu.soomsil.usaint.screen.login.navigation.Login
+import com.yourssu.soomsil.usaint.screen.navigation.USaintNavHost
 import com.yourssu.soomsil.usaint.ui.theme.SoomsilUSaintTheme
 import dev.eatsteak.rusaint.ffi.USaintSessionBuilder
 
@@ -19,34 +17,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
+            val navController: NavHostController = rememberNavController()
+
             LaunchedEffect(Unit) {
                 USaintSessionBuilder().withPassword("20211722", "kwakkun1208!")
             }
             SoomsilUSaintTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                YdsScaffold() {
+                    USaintNavHost(
+                        navController = navController,
+                        startDestination = Login
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SoomsilUSaintTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/navigation/USaintNavHost.kt
@@ -0,0 +1,16 @@
+package com.yourssu.soomsil.usaint.screen.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.yourssu.soomsil.usaint.screen.login.navigation.Login
+
+@Composable
+fun USaintNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    startDestination: Any = Login,
+) {
+
+
+}
