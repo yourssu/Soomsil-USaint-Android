4630091fa05bf6460e43253ce1edfbe22e6e3c72
kwak kun <gael.urssu@gmail.com>
2024-12-17 22:56:47 +0900

    add : di module 추가

---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/di/DataBaseModule.kt
@@ -0,0 +1,25 @@
+package com.yourssu.soomsil.usaint.di
+
+import android.content.Context
+import androidx.room.Room
+import com.yourssu.soomsil.usaint.data.source.local.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataBaseModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "my_database"
+        ).build()
+    }
+}
