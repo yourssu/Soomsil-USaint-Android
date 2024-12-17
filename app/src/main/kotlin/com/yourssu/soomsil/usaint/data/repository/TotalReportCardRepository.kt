055825fec5b02ccf1c067353b832d2b841594c26
kwak kun <gael.urssu@gmail.com>
2024-12-18 01:10:02 +0900

    add : repository 구성

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/data/repository/TotalReportCardRepository.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/data/repository/TotalReportCardRepository.kt
@@ -1,14 +1,31 @@
 package com.yourssu.soomsil.usaint.data.repository
 
+import com.yourssu.soomsil.usaint.data.source.local.dao.LectureDao
+import com.yourssu.soomsil.usaint.data.source.local.dao.SemesterDao
 import com.yourssu.soomsil.usaint.data.source.local.dao.TotalReportCardDao
+import com.yourssu.soomsil.usaint.data.source.local.entity.Lecture
+import com.yourssu.soomsil.usaint.data.source.local.entity.Semester
 import com.yourssu.soomsil.usaint.data.source.local.entity.TotalReportCard
+import com.yourssu.soomsil.usaint.data.source.local.entity.TotalReportCardWithSemesters
 
-class TotalReportCardRepository(
-    private val totalReportCardDao: TotalReportCardDao
+class ReportCardRepository(
+    private val totalReportCardDao: TotalReportCardDao,
+    private val semesterDao: SemesterDao,
+    private val lectureDao: LectureDao
 ) {
-    suspend fun getTotalReportCard() = totalReportCardDao.getTotalReportCard()
+    fun insertTotalReportCard(totalReportCard: TotalReportCard): Int {
+        return totalReportCardDao.insertTotalReportCard(totalReportCard).toInt()
+    }
 
-    suspend fun insertTotalReportCard(earnedCredit: Float, gpa: Float) {
-        totalReportCardDao.insertTotalReportCard(totalReportCard = TotalReportCard(earnedCredit = earnedCredit, gpa = gpa))
+    fun insertSemesters(vararg semesters: Semester): List<Int> {
+        return semesterDao.insertSemesters(*semesters).map { it.toInt() }
+    }
+
+    fun insertLectures(vararg lectures: Lecture): List<Int> {
+        return lectureDao.insertLectures(*lectures).map { it.toInt() }
+    }
+
+    fun getTotalReportCardWithAllData(id: Int): TotalReportCardWithSemesters? {
+        return totalReportCardDao.getTotalReportCardWithSemesters(id)
     }
 }
