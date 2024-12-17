414e7f69d20a3c4aca9fdc453fea32a5158f058e
kwak kun <gael.urssu@gmail.com>
2024-12-18 04:04:53 +0900

    chore : 테스트 코드 작성함

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,60 +15,41 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yourssu.soomsil.usaint.ui.theme.SoomsilUSaintTheme
+import dagger.hilt.android.AndroidEntryPoint
 import dev.eatsteak.rusaint.core.CourseType
 import dev.eatsteak.rusaint.core.SemesterType
 import dev.eatsteak.rusaint.ffi.CourseGradesApplicationBuilder
 import dev.eatsteak.rusaint.ffi.StudentInformationApplicationBuilder
 import dev.eatsteak.rusaint.ffi.USaintSessionBuilder
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: ReportCardViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        val db = Room.databaseBuilder(
-//            applicationContext,
-//            AppDatabase::class.java,
-//            "my_database"
-//        ).build()
-//
-//        val dao = db.gradeDao()
-//
-//        // Repository 인스턴스 생성
-//        val repository = TotalReportCardRepository(
-//            totalReportCardDao = dao
-//        )
-//
-//        lifecycleScope.launch {
-//            // 데이터베이스에 데이터 삽입
-//            repository.insertTotalReportCard(earnedCredit = 100.0f, gpa = 4.5f)
-//
-//            // 데이터베이스에서 데이터 조회
-//            val totalReportCard = repository.getTotalReportCard()
-//            Log.d("MainActivity", totalReportCard.toString())
-//        }
-
 
         enableEdgeToEdge()
         setContent {
             LaunchedEffect(Unit) {
-                val session = USaintSessionBuilder().withPassword("20211722", "kwakkun1208!")
-                val courseGradesApplication = CourseGradesApplicationBuilder().build(session)
-                val studentInformationApplication = StudentInformationApplicationBuilder().build(session)
 
-                Log.d("MainActivity", courseGradesApplication.semesters(CourseType.BACHELOR).toString())
-                Log.d(
-                    "MainActivity",
-                    courseGradesApplication.classes(CourseType.BACHELOR, "2021", SemesterType.ONE, false)
-                        .toString()
-                )
-                Log.d(
-                    "MainActivity",
-                    courseGradesApplication.classes(CourseType.BACHELOR, "2021", SemesterType.TWO, true)
-                        .toString()
-                )
-                Log.d("MainActivity", studentInformationApplication.general().toString())
-                Log.d("MainActivity", studentInformationApplication.family().toString())
-                Log.d("MainActivity", studentInformationApplication.work().toString())
+//                viewModel.testOperations()
+                viewModel.testDuplicateHandling()
+
+//                val session = USaintSessionBuilder().withPassword("20211722", "kwakkun1208!")
+//                val courseGradesApplication = CourseGradesApplicationBuilder().build(session)
+//                val studentInformationApplication = StudentInformationApplicationBuilder().build(session)
+
+//                Log.d("MainActivity", courseGradesApplication.semesters(CourseType.BACHELOR).toString())
+//                Log.d(
+//                    "MainActivity",
+//                    courseGradesApplication.classes(CourseType.BACHELOR, "2021", SemesterType.TWO, true)
+//                        .toString()
+//                )
+//                Log.d("MainActivity", studentInformationApplication.general().toString())
+//                Log.d("MainActivity", studentInformationApplication.family().toString())
+//                Log.d("MainActivity", studentInformationApplication.work().toString())
             }
             SoomsilUSaintTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
