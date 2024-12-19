1b85578ab91c4fd134a1cc52e0f7f3b320170f1e
yuri <83583757+kangyuri1114@users.noreply.github.com>
2024-12-19 13:18:55 +0900

    feat: settingScreen UI 구현 (#10)
    ![image](https://github.com/user-attachments/assets/e7186fe2-69e6-4ee7-8c3b-5fa726721aca)

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/setting/SettingScreen.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/setting/SettingScreen.kt
@@ -1,14 +1,30 @@
 package com.yourssu.soomsil.usaint.screen.setting
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.yourssu.design.system.atom.Text
+import androidx.compose.ui.unit.dp
 import com.yourssu.design.system.compose.YdsTheme
 import com.yourssu.design.system.compose.atom.ListItem
+import com.yourssu.design.system.compose.atom.Toggle
 import com.yourssu.design.system.compose.atom.TopBarButton
 import com.yourssu.design.system.compose.base.YdsScaffold
+import com.yourssu.design.system.compose.base.YdsText
+import com.yourssu.design.system.compose.component.List
 import com.yourssu.design.system.compose.component.topbar.TopBar
 import com.yourssu.soomsil.usaint.R
 import com.yourssu.design.R as YdsR
@@ -32,16 +48,69 @@ fun SettingScreen(
             )
         },
     ) {
-        com.yourssu.design.system.compose.component.List {
-            item {
-                ListItem(
-                    text = stringResource(id = R.string.setting_logout),
-                    onClick = {
-                        // viewModel.logout()
-                        // activity.toast("로그아웃 되었습니다.")
-                        onBackClick()
-                    },
-                )
+        Column(
+            modifier = modifier
+        ) {
+            YdsText(
+                text = stringResource(id = R.string.setting),
+                style = YdsTheme.typography.title1,
+                modifier = modifier.padding(start = 16.dp, top = 6.dp, bottom = 8.dp),
+            )
+
+            List(subHeader = stringResource(R.string.manage_account)) {
+                item {
+                    ListItem(
+                        text = stringResource(id = R.string.setting_logout),
+                        onClick = {
+                            // TODO
+                            onBackClick()
+                        },
+                    )
+                }
+            }
+
+            List(subHeader = stringResource(R.string.alarm)) {
+                item {
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .height(48.dp)
+                            .padding(horizontal = 20.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        YdsText(
+                            text = stringResource(R.string.get_alarm),
+                            style = YdsTheme.typography.body1,
+                        )
+
+                        var checked1 by remember { mutableStateOf(false) } // TODO
+                        Toggle(
+                            checked = checked1, // TODO
+                            onCheckedChange = {
+                                checked1 = it // TODO
+                            },
+                        )
+                    }
+                }
+            }
+
+            List(subHeader = stringResource(R.string.terms_title)) {
+                item {
+                    ListItem(
+                        text = stringResource(R.string.terms_of_service),
+                        onClick = {
+                            // TODO
+                        },
+                    )
+
+                    ListItem(
+                        text = stringResource(R.string.terms_of_privacy_info),
+                        onClick = {
+                            // TODO
+                        },
+                    )
+                }
             }
         }
     }
@@ -50,5 +119,7 @@ fun SettingScreen(
 @Preview
 @Composable
 fun PreviewSettingScreen(){
-    SettingScreen(onBackClick = {})
+    YdsTheme{
+        SettingScreen(onBackClick = {})
+    }
 }
