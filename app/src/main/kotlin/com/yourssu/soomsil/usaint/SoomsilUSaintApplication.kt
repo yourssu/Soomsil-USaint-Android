a2b5e064a15899fec874b82d74c100e3e7354abe
cometj03 <harryjung1001@gmail.com>
2024-12-21 11:18:29 +0900

    chore: Timber enable

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/SoomsilUSaintApplication.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/SoomsilUSaintApplication.kt
@@ -2,6 +2,14 @@ package com.yourssu.soomsil.usaint
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class SoomsilUSaintApplication : Application()
+class SoomsilUSaintApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
