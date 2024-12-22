213046ae7a4c58f8d0654fc548be5d749db7702e
cometj03 <harryjung1001@gmail.com>
2024-12-22 09:40:27 +0900

    chore: versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.yourssu.soomsil.usaint"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
