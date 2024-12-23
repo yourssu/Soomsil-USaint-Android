d733c2918b5dc112e2db3ad5217234994725b2a1
Gael-Android <gael.urssu@gmail.com>
2024-12-23 17:14:35 +0900

    add : 메인 엑티비티에 worker 등록코드 추가

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Update
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.yourssu.design.system.compose.YdsTheme
 import com.yourssu.soomsil.usaint.screen.USaintNavHost
 import com.yourssu.soomsil.usaint.screen.home.navigation.Home
 import com.yourssu.soomsil.usaint.screen.login.navigation.Login
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -18,6 +24,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
+
+        // 네트워트 연결시에만 실행하는 제약
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        // 3시간에 한번씩 Worker 실행
+        val periodicRequest = PeriodicWorkRequestBuilder<UpdateWorker>(3, TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .build()
+
+        // WorkManager에 등록 -> 앱이 백그라운드/포그라운드여도 시스템이 주기적으로 실행
+        WorkManager.getInstance(this).enqueue(periodicRequest)
 
         setContent {
             YdsTheme(
