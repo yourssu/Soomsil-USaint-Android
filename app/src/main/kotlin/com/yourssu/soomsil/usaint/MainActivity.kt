5cd42d3a9fc920d19dcb1d3829343c2ba5e37e8d
Gael-Android <gael.urssu@gmail.com>
2024-12-23 18:06:53 +0900

    add : 테스트 할때는 시작하기 전에 모든 워커를 취소, 유니크 워커로 단 하나의 워커로 강제, 여러번 실행을 위해서 캔슬 후 리인큐

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.viewModels
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Update
 import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
@@ -15,6 +16,7 @@ import com.yourssu.soomsil.usaint.screen.USaintNavHost
 import com.yourssu.soomsil.usaint.screen.home.navigation.Home
 import com.yourssu.soomsil.usaint.screen.login.navigation.Login
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
@@ -24,6 +26,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
+
+        // 테스트 목적으로 모든 워커를 취소
+        WorkManager.getInstance(this).cancelAllWork()
 
         // 네트워트 연결시에만 실행하는 제약
         val constraints = Constraints.Builder()
@@ -35,8 +40,17 @@ class MainActivity : ComponentActivity() {
             .setConstraints(constraints)
             .build()
 
+        // 1734944395256
+        //
+
         // WorkManager에 등록 -> 앱이 백그라운드/포그라운드여도 시스템이 주기적으로 실행
-        WorkManager.getInstance(this).enqueue(periodicRequest)
+        // 유니크한 이름으로 등록 -> 이미 등록된 이름이 있으면 KEEP으로 유지(동일한 여러개 등록 방지)
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "UpdateWorker", // 유니크한 이름
+            ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE,
+            periodicRequest // 실행할 Worker
+        )
+        Timber.d("WorkManager: enqueueUniquePeriodicWork time : ${getCurrentTimeInHoursAndMinutes()}")
 
         setContent {
             YdsTheme(
