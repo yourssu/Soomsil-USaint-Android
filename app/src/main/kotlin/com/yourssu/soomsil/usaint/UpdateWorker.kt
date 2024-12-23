5929bf95517ed4fe60d5dc6b6a0dfd782b825f8d
Gael-Android <gael.urssu@gmail.com>
2024-12-23 17:08:09 +0900

    add : 예시 worker 만듬

---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/UpdateWorker.kt
@@ -0,0 +1,57 @@
+package com.yourssu.soomsil.usaint
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+class UpdateWorker(
+    appContext: Context,
+    workerParams: WorkerParameters
+) : CoroutineWorker(appContext, workerParams) {
+
+//    // 원하는 작업을 수행하는 Repository를 생성
+//    private val testRepository = TestRepository()
+
+    override suspend fun doWork(): Result {
+        // 작업 수행
+
+        // 알림 띄우기
+        showNotification("WorkManager", "WorkManager is working!") // 알림 발행
+
+        return Result.success()
+    }
+
+    private fun showNotification(title: String, message: String) {
+        val notificationManager = applicationContext.getSystemService(
+            Context.NOTIFICATION_SERVICE
+        ) as NotificationManager
+
+        val channelId = "work_manager_channel"
+        val channelName = "WorkManagerPushChannel"
+
+        val channel = NotificationChannel(
+            channelId,
+            channelName,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+
+
+        val notificationBuilder = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.app_icon)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        // 알림 발행
+        notificationManager.notify(
+            System.currentTimeMillis().toInt(),
+            notificationBuilder.build()
+        )
+    }
+}
