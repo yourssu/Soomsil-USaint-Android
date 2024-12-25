ccf833ccdf56018a08ebcd340667b1e59426e653
kwak kun <gael.urssu@gmail.com>
2024-12-25 20:42:00 +0900

    bugfix : 로딩 시 다른 화면으로 이동할 때 중지시키도록 수정

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/home/HomeViewModel.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/home/HomeViewModel.kt
@@ -17,6 +17,8 @@ import com.yourssu.soomsil.usaint.ui.entities.toReportCardSummary
 import com.yourssu.soomsil.usaint.ui.entities.toStudentInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.eatsteak.rusaint.ffi.RusaintException
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
@@ -39,69 +41,102 @@ class HomeViewModel @Inject constructor(
     var reportCardSummary: ReportCardSummary by mutableStateOf(ReportCardSummary())
         private set
 
+    // job 정의
+    private var initJob: Job? = null
+    private var refreshJob: Job? = null
+
     init {
         initialize()
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        // ViewModel이 소멸될 때 Job도 함께 취소
+        cancelJob()
+    }
+
+    fun cancelJob() {
+        Timber.d("HomeViewModel cancelJob")
+        isRefreshing = false
+        initJob?.cancel()
+        refreshJob?.cancel()
+    }
+
     fun refresh() {
-        viewModelScope.launch {
-            isRefreshing = true
-            val session = uSaintSessionRepo.getSession().getOrElse { e ->
-                Timber.e(e)
-                _uiEvent.emit(UiEvent.SessionFailure)
-                isRefreshing = false
-                return@launch
-            }
-            val stuDto = studentInfoRepo.getRemoteStudentInfo(session).getOrElse { e ->
-                Timber.e(e)
-                when (e) {
-                    is RusaintException -> _uiEvent.emit(UiEvent.RefreshFailure)
-                    else -> _uiEvent.emit(UiEvent.Failure())
+        // 이전에 진행 중이던 refreshJob이 있으면 취소
+        refreshJob?.cancel()
+
+        refreshJob = viewModelScope.launch {
+            try {
+                isRefreshing = true
+                val session = uSaintSessionRepo.getSession().getOrElse { e ->
+                    Timber.e(e)
+                    _uiEvent.emit(UiEvent.SessionFailure)
+                    isRefreshing = false
+                    return@launch
                 }
-                isRefreshing = false
-                return@launch
-            }
-            val totalReportCard = totalReportCardRepo.getRemoteReportCard(session).getOrElse { e ->
-                Timber.e(e)
-                val errMsg = when (e) {
-                    is RusaintException -> "새로고침에 실패했습니다. 다시 시도해주세요."
-                    else -> "알 수 없는 문제가 발생했습니다."
+                val stuDto = studentInfoRepo.getRemoteStudentInfo(session).getOrElse { e ->
+                    Timber.e(e)
+                    when (e) {
+                        is RusaintException -> _uiEvent.emit(UiEvent.RefreshFailure)
+                        else -> _uiEvent.emit(UiEvent.Failure())
+                    }
+                    isRefreshing = false
+                    return@launch
                 }
-                _uiEvent.emit(UiEvent.Failure(errMsg))
+                val totalReportCard =
+                    totalReportCardRepo.getRemoteReportCard(session).getOrElse { e ->
+                        Timber.e(e)
+                        val errMsg = when (e) {
+                            is RusaintException -> "새로고침에 실패했습니다. 다시 시도해주세요."
+                            else -> "알 수 없는 문제가 발생했습니다."
+                        }
+                        _uiEvent.emit(UiEvent.Failure(errMsg))
+                        isRefreshing = false
+                        return@launch
+                    }
+                // ui state 변경
+                studentInfo = StudentInfo(
+                    name = stuDto.name,
+                    department = stuDto.department,
+                    grade = stuDto.grade.toInt(),
+                )
+                reportCardSummary = ReportCardSummary(
+                    gpa = totalReportCard.gpa.toGrade(),
+                    earnedCredit = totalReportCard.earnedCredit.toCredit(),
+                    graduateCredit = totalReportCard.graduateCredit.toCredit(),
+                )
+                // DB 갱신
+                totalReportCardRepo.storeReportCard(totalReportCard).onFailure { e -> Timber.e(e) }
+                studentInfoRepo.storeStudentInfo(stuDto).onFailure { e -> Timber.e(e) }
+                _uiEvent.emit(UiEvent.Success)
                 isRefreshing = false
-                return@launch
+            } catch (e:CancellationException) {
+                Timber.e("Job was cancelled: $e")
             }
-            // ui state 변경
-            studentInfo = StudentInfo(
-                name = stuDto.name,
-                department = stuDto.department,
-                grade = stuDto.grade.toInt(),
-            )
-            reportCardSummary = ReportCardSummary(
-                gpa = totalReportCard.gpa.toGrade(),
-                earnedCredit = totalReportCard.earnedCredit.toCredit(),
-                graduateCredit = totalReportCard.graduateCredit.toCredit(),
-            )
-            // DB 갱신
-            totalReportCardRepo.storeReportCard(totalReportCard).onFailure { e -> Timber.e(e) }
-            studentInfoRepo.storeStudentInfo(stuDto).onFailure { e -> Timber.e(e) }
-            _uiEvent.emit(UiEvent.Success)
-            isRefreshing = false
         }
     }
 
     private fun initialize() {
-        viewModelScope.launch {
-            studentInfoRepo.getLocalStudentInfo()
-                .onSuccess { stu ->
-                    studentInfo = stu.toStudentInfo()
-                }
-                .onFailure { e -> Timber.e(e) }
-            totalReportCardRepo.getLocalReportCard()
-                .onSuccess { totalReportCard ->
-                    reportCardSummary = totalReportCard.toReportCardSummary()
-                }
-                .onFailure { e -> Timber.e(e) }
+
+        // 이전에 진행 중이던 initJob이 있으면 취소
+        initJob?.cancel()
+
+        initJob = viewModelScope.launch {
+            try {
+                studentInfoRepo.getLocalStudentInfo()
+                    .onSuccess { stu ->
+                        studentInfo = stu.toStudentInfo()
+                    }
+                    .onFailure { e -> Timber.e(e) }
+                totalReportCardRepo.getLocalReportCard()
+                    .onSuccess { totalReportCard ->
+                        reportCardSummary = totalReportCard.toReportCardSummary()
+                    }
+                    .onFailure { e -> Timber.e(e) }
+            } catch (e: CancellationException) {
+                Timber.e("Job was cancelled: $e")
+            }
         }
     }
 }
