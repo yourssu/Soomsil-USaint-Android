312c3f19a88e4aa88768fd37dbcc0fd3160937f6
최종수 <chlwhdtn03@naver.com>
2025-04-12 06:05:08 +0900

    ChapelCardItem 생성 (#57)
    ## 📝작업 내용
    
    > 기존 ChapelCardItem 주석 삭제하고 새로 만들었습니다.

---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/ui/entities/Chapel.kt
@@ -0,0 +1,8 @@
+package com.yourssu.soomsil.usaint.ui.entities
+
+data class ChapelInfo(
+    val seat: String,
+    val time: String,
+    val totalAttendance: Int,
+    val currentAttendance: Int
+)
