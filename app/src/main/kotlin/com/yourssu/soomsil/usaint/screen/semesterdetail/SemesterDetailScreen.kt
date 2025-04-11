fafd7f7b9afeed15cdea859e04143dbca56c7c2f
cometj03 <harryjung1001@gmail.com>
2025-04-11 22:02:26 +0900

    chore: replace icon to Material's

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/semesterdetail/SemesterDetailScreen.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/semesterdetail/SemesterDetailScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
+import androidx.compose.material.icons.outlined.AddPhotoAlternate
 import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -38,10 +39,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
@@ -181,8 +180,8 @@ fun SemesterDetailScreen(
                     actions = {
                         IconButton(onClick = { showBottomSheet = true }) {
                             Icon(
-                                imageVector = ImageVector.vectorResource(R.drawable.outline_add_photo_alternate_24),
-                                contentDescription = "add to photo",
+                                imageVector = Icons.Outlined.AddPhotoAlternate,
+                                contentDescription = "Add to photo",
                             )
                         }
                         IconButton(onClick = {
