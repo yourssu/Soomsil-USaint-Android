6ce70b652f2ab4cd6d2a7041715f8cfad3add813
Jung Haesung <harryjung1001@gmail.com>
2025-04-13 23:10:13 +0900

    [feat] 바텀 네비게이션 추가 (#61)
    ## #️⃣연관된 이슈
    X
    
    ## 📝작업 내용
    
    > 이번 PR에서 작업한 내용을 간략히 설명해주세요(이미지 첨부 가능)
    
    - 새로운 디자인에 따라 바텀 네비게이션 추가
    - 비어있는 ReportCardScreen, ChapelScreen 추가
    - [Now In
    Android](https://github.com/android/nowinandroid/blob/main/app/src/main/kotlin/com/google/samples/apps/nowinandroid/ui/NiaApp.kt)의
    코드를 참고했습니다.
    
    
    https://github.com/user-attachments/assets/5b9c40c1-5f68-47f8-ba92-0273038be1a2
    
    
    ## 💬리뷰 요구사항(선택)
    
    > 리뷰어가 특별히 봐주었으면 하는 부분이 있다면 작성해주세요
    >
    > ex) 메서드 XXX의 이름을 더 잘 짓고 싶은데 혹시 좋은 명칭이 있을까요?

---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/navigation/TopLevelDestination.kt
@@ -0,0 +1,67 @@
+package com.yourssu.soomsil.usaint.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Church
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.School
+import androidx.compose.material.icons.outlined.Church
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.School
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.navOptions
+import com.yourssu.soomsil.usaint.screen.chapel.navigation.Chapel
+import com.yourssu.soomsil.usaint.screen.chapel.navigation.navigateToChapel
+import com.yourssu.soomsil.usaint.screen.home.navigation.Home
+import com.yourssu.soomsil.usaint.screen.home.navigation.navigateToHome
+import com.yourssu.soomsil.usaint.screen.reportcard.navigation.ReportCard
+import com.yourssu.soomsil.usaint.screen.reportcard.navigation.navigateToReportCard
+import kotlin.reflect.KClass
+
+enum class TopLevelDestination(
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val label: String,
+    val route: KClass<*>,
+) {
+    HOME(
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home,
+        label = "홈",
+        route = Home::class,
+    ),
+    REPORT_CARD(
+        selectedIcon = Icons.Filled.School,
+        unselectedIcon = Icons.Outlined.School,
+        label = "성적",
+        route = ReportCard::class,
+    ),
+    CHAPEL(
+        selectedIcon = Icons.Filled.Church,
+        unselectedIcon = Icons.Outlined.Church,
+        label = "채플",
+        route = Chapel::class,
+    )
+}
+
+fun NavHostController.navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+    val topLevelNavOptions = navOptions {
+        // Pop up to the start destination of the graph to
+        // avoid building up a large stack of destinations
+        // on the back stack as users select items
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
+        // Avoid multiple copies of the same destination when
+        // reselecting the same item
+        launchSingleTop = true
+        // Restore state when reselecting a previously selected item
+        restoreState = true
+    }
+    when (topLevelDestination) {
+        TopLevelDestination.HOME -> this.navigateToHome(topLevelNavOptions)
+        TopLevelDestination.REPORT_CARD -> this.navigateToReportCard(topLevelNavOptions)
+        TopLevelDestination.CHAPEL -> this.navigateToChapel(topLevelNavOptions)
+    }
+}
