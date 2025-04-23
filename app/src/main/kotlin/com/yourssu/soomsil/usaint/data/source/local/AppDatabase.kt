90a7c3ec9290d095340249d5968532c140b7a73e
cometj03 <harryjung1001@gmail.com>
2025-04-24 00:11:49 +0900

    feat: AutoMigration 1 to 2, 2 to 3

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/data/source/local/AppDatabase.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/data/source/local/AppDatabase.kt
@@ -13,6 +13,8 @@ import com.yourssu.soomsil.usaint.data.source.local.entity.SemesterEntity
     entities = [SemesterEntity::class, LectureEntity::class],
     version = 4,
     autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4, spec = DatabaseMigrations.Schema3to4::class),
     ],
     exportSchema = true,
