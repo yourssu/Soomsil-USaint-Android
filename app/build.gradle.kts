293a0f61a5782b6be19facd8deeec9e3aabd3036
cometj03 <harryjung1001@gmail.com>
2025-05-01 16:13:16 +0900

    [version update] 0.2.0 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.yourssu.soomsil.usaint"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.0"
+        versionCode = 8
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
