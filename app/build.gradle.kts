43a1646127dfa3c42e3c5a8205cd9e8f72a37c76
cometj03 <harryjung1001@gmail.com>
2025-05-02 02:26:40 +0900

    chore: 링크 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.yourssu.soomsil.usaint"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
