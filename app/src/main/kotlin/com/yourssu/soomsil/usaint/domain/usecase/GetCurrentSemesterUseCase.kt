3910819e562222f00bd92d98f2d1cd5c46db095e
cometj03 <harryjung1001@gmail.com>
2025-05-12 18:31:55 +0900

    fix: 학기 일정 수정 및 연도도 반환

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/domain/usecase/GetCurrentSemesterUseCase.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/domain/usecase/GetCurrentSemesterUseCase.kt
@@ -5,20 +5,32 @@ import java.time.LocalDate
 import javax.inject.Inject
 
 class GetCurrentSemesterUseCase @Inject constructor() {
-    operator fun invoke(): SemesterType? {
+    operator fun invoke(): Pair<Int, SemesterType>? {
         val now = LocalDate.now()
         val year = now.year
 
-        // 1학기: X년도 6/8 ~ X년도 7/7
-        // 여름학기: X년도 7/11 ~ X년도 7/25
-        // 2학기: X년도 12/8 ~ X+1년도 1/7
-        // 겨울학기: X+1년도 1/11 ~ X+1년도 1/26
+        // 2025년도 학기 개강 ~ 종강
+        // https://ssu.ac.kr/%ED%95%99%EC%82%AC/%ED%95%99%EC%82%AC%EC%9D%BC%EC%A0%95/
+        // 1학기: 3/4 ~ 6/23
+        // 여름학기: 6/24 ~ 7/14
+        // 2학기: 9/1 ~ 12/20
+        // 겨울학기: 12/22 ~ 1/15
         return when (now) {
-            in LocalDate.of(year, 6, 8)..LocalDate.of(year, 7, 7) -> SemesterType.One
-            in LocalDate.of(year, 7, 11)..LocalDate.of(year, 7, 25) -> SemesterType.Summer
-            in LocalDate.of(year, 12, 8)..LocalDate.of(year, 12, 31) -> SemesterType.Two
-            in LocalDate.of(year, 1, 1)..LocalDate.of(year, 1, 7) -> SemesterType.Two
-            in LocalDate.of(year, 1, 11)..LocalDate.of(year, 1, 26) -> SemesterType.Winter
+            in LocalDate.of(year, 3, 4)..LocalDate.of(year, 6, 23) ->
+                Pair(year, SemesterType.One)
+
+            in LocalDate.of(year, 6, 24)..LocalDate.of(year, 7, 14) ->
+                Pair(year, SemesterType.Summer)
+
+            in LocalDate.of(year, 9, 1)..LocalDate.of(year, 12, 20) ->
+                Pair(year, SemesterType.Two)
+
+            in LocalDate.of(year, 12, 22)..LocalDate.of(year, 12, 31) ->
+                Pair(year, SemesterType.Winter)
+
+            in LocalDate.of(year, 1, 1)..LocalDate.of(year, 1, 15) ->
+                Pair(year - 1, SemesterType.Winter)
+
             else -> null
         }
     }
