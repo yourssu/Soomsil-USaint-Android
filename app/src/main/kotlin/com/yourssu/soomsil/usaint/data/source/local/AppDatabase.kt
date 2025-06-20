0e1c5dce8acbba7b5bd21eaccfe19e05cadef50d
minjun011026 <jun011026@gmail.com>
2025-06-20 15:19:41 +0900

    refactor: room-version-4-to-5

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/data/source/local/AppDatabase.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/data/source/local/AppDatabase.kt
@@ -14,11 +14,12 @@ import com.yourssu.soomsil.usaint.data.source.local.entity.SemesterEntity
 
 @Database(
     entities = [SemesterEntity::class, LectureEntity::class, ChapelEntity::class, ChapelAttendanceEntity::class],
-    version = 4,
+    version = 5,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
         AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4, spec = DatabaseMigrations.Schema3to4::class),
+        AutoMigration(from = 4, to = 5)
     ],
     exportSchema = true,
 )
