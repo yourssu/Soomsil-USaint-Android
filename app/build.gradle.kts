7936e796113185377d13d10e0b9262914e7966fc
minjun011026 <jun011026@gmail.com>
2025-06-21 23:43:27 +0900

    feat: version-up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.yourssu.soomsil.usaint"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.2.2"
+        versionCode = 16
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
