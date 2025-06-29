42de9ddcff60085f70718b12d3e7ed06f5d31047
minjun011026 <jun011026@gmail.com>
2025-06-29 20:10:05 +0900

    feat: rusaint-version-up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.yourssu.soomsil.usaint"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.2.3"
+        versionCode = 17
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
