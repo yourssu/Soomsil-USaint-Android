345156b0e0b1fdb777d4d0bd18f669342c01ee01
minjun011026 <jun011026@gmail.com>
2025-07-02 15:52:01 +0900

    version update: 0.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.yourssu.soomsil.usaint"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.2.4"
+        versionCode = 18
+        versionName = "0.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
