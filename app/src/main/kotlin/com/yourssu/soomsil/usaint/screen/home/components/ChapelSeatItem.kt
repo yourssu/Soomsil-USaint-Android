cb6c3219a7c9f3d0fcaeed929a46e9fb4d527850
jun011026 <jun011026@gmail.com>
2025-09-24 14:12:19 +0900

    feat: chapelCard-animation-setting

---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/home/components/ChapelSeatItem.kt
@@ -0,0 +1,12 @@
+package com.yourssu.soomsil.usaint.screen.home.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChapelSeatItem(
+    seatNumer: String,
+    modifier: Modifier = Modifier,
+) {
+
+}
