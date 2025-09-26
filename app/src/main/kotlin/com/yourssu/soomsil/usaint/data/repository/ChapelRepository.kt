d447fd987a8d29f5f9d573390de85211e14e5891
최종수 <chlwhdtn03@naver.com>
2025-09-26 13:00:14 +0900

    combine 사용

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/data/repository/ChapelRepository.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/data/repository/ChapelRepository.kt
@@ -14,8 +14,6 @@ import com.yourssu.soomsil.usaint.data.source.local.entity.asExternalModel
 import com.yourssu.soomsil.usaint.data.source.remote.USaintRemoteSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ChapelRepository @Inject constructor(
@@ -40,35 +38,41 @@ class ChapelRepository @Inject constructor(
                 val compositeKey = Triple(chapel.year, chapel.semester, chapel.division)
                 ChapelData(
                     chapelSimpleData = chapel.asExternalModel(),
-                    chapelAttendances = attendancesMap[compositeKey]?.map(ChapelAttendanceEntity::asExternalModel) ?: emptyList()
+                    chapelAttendances = attendancesMap[compositeKey]?.map(ChapelAttendanceEntity::asExternalModel)
+                        ?: emptyList()
                 )
             }
         }
 
     // getCurrentSemesterUseCase를 사용하기가 어려워서
     // 전체 채플 데이터에서 DataStore에 저장한 분반 정보를 비교하여 뽑아냅니다.
-    val chapelCard: Flow<ChapelData?> = // 현재 학기 설정에 따른 카드에 표시할 채플정보
-        chapels.map { chapelList ->
+    val chapelCard: Flow<ChapelData?> =
+        combine(chapels, chapelDataSource.chapelCardData) { chapelList, cardData ->
             chapelList.find {
-                it.chapelSimpleData.year == chapelDataSource.chapelCardData.first().year &&
-                        it.chapelSimpleData.semester == chapelDataSource.chapelCardData.first().semester
+                it.chapelSimpleData.year == cardData.year &&
+                        it.chapelSimpleData.semester == cardData.semester
             }
         }
 
     // 앱이 최초에 실행될 때 이 함수가 먼저 실행됩니다.
     // 채플 DB와 DataStore에 불러온 값을 저장합니다.
-    suspend fun fetchChapelCardData(specifiedCurrentSemester: Pair<Int, SemesterType>): Result<Unit> = runCatching {
-        val credential = studentCredential.getStudentCredential()
-        val chapelCardData = uSaintRemoteSource.remoteChapelData(
-            credential,
-            specifiedCurrentSemester.first,
-            specifiedCurrentSemester.second
-        )
+    suspend fun fetchChapelCardData(specifiedCurrentSemester: Pair<Int, SemesterType>): Result<Unit> =
+        runCatching {
+            val credential = studentCredential.getStudentCredential()
+            val chapelCardData = uSaintRemoteSource.remoteChapelData(
+                credential,
+                specifiedCurrentSemester.first,
+                specifiedCurrentSemester.second
+            )
 
-        chapelDataSource.setChapelCardData(chapelSimpleData = chapelCardData.chapelSimpleData)
-        chapelDao.upsertChapel(chapelCardData.chapelSimpleData.asEntity())
-        chapelDao.upsertChapelAttendances(chapelCardData.chapelAttendances.map(ChapelAttendanceData::asEntity))
-    }
+            chapelDataSource.setChapelCardData(chapelSimpleData = chapelCardData.chapelSimpleData)
+            chapelDao.upsertChapel(chapelCardData.chapelSimpleData.asEntity())
+            chapelDao.upsertChapelAttendances(
+                chapelCardData.chapelAttendances.map(
+                    ChapelAttendanceData::asEntity
+                )
+            )
+        }
 
     // 현재 학기를 제외한 이수한 학기의 채플 정보를 모두 불러옵니다.
     // 채플 DB에 불러온 값을 저장합니다.
@@ -85,8 +89,11 @@ class ChapelRepository @Inject constructor(
                 )
 
                 chapelDao.upsertChapel(chapelData.chapelSimpleData.asEntity())
-                chapelDao.upsertChapelAttendances(chapelData.chapelAttendances.map(
-                    ChapelAttendanceData::asEntity))
+                chapelDao.upsertChapelAttendances(
+                    chapelData.chapelAttendances.map(
+                        ChapelAttendanceData::asEntity
+                    )
+                )
             } catch (e: Exception) {
                 // 계절학기, 6회 수강 완료 등 채플 데이터가 없는 학기에 대해서는
                 // 에러가 발생하지만 동작이 멈추면 안되고 다음 학기를 검색해야함
