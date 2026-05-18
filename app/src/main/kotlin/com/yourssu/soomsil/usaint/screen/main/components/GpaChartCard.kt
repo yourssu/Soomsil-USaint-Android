a5997db2c3fc2c8f3b4a61ed2431621fc9714380
findwhatiwant <jws0041@naver.com>
2026-05-18 22:43:15 +0900

    [fix] MainScreen 컴포넌트(ChapleCard, GpaChartCard) import 에러 해결 ## #️⃣연관된 이슈 X
    ## 📝작업 내용
    
    > 이번 PR에서 작업한 내용을 간략히 설명해주세요(이미지 첨부 가능)
    
    MainScreen 컴포넌트(ChapleCard, GpaChartCard) import 에러 해결. 안쓰는 weight 관련 import 삭제

--- app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/main/components/GpaChartCard.kt
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/main/components/GpaChartCard.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
