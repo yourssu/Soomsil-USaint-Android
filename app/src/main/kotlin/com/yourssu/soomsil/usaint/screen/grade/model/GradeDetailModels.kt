d60aa455a0f9f60862f749bdab89837b57d21217
findwhatiwant <jws0041@naver.com>
2026-05-18 22:21:24 +0900

    [feat] GradeDetail 추가 ## #️⃣연관된 이슈 X
    ## 📝작업 내용
    
    > 이번 PR에서 작업한 내용을 간략히 설명해주세요(이미지 첨부 가능)
    
    컴포넌트화 한 상태로 커밋. 추후에 아이콘 추가 필요

---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/grade/model/GradeDetailModels.kt
@@ -0,0 +1,24 @@
+package com.yourssu.soomsil.usaint.screen.grade.model
+
+import androidx.compose.ui.graphics.Color
+
+data class SemesterTab(
+    val label: String,
+    val isActive: Boolean = false
+)
+
+data class GpaPoint(
+    val semester: String,
+    val gpa: Float,
+    val isCurrent: Boolean = false
+)
+
+data class CourseItem(
+    val name: String,
+    val professor: String,
+    val credit: String,
+    val grade: String,
+    val dotColor: Color,
+    val gradeColor: Color,
+    val badgeBgColor: Color
+)
