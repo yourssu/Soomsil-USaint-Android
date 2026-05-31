d6020d95b033d57d6ec079d429ec0ccbd65e2b95
findwhatiwant <jws0041@naver.com>
2026-05-31 19:18:38 +0900

    [feat] 뷰모델 추가 ## #️⃣연관된 이슈 X
    ## 📝작업 내용
    
    > 이번 PR에서 작업한 내용을 간략히 설명해주세요(이미지 첨부 가능)
    빈 뷰모델 추가

---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/grade/GradeViewModel.kt
@@ -0,0 +1,8 @@
+package com.yourssu.soomsil.usaint.screen.grade
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GradeViewModel @Inject constructor() : ViewModel()
---
+++ app/src/main/kotlin/com/yourssu/soomsil/usaint/screen/pushnotifications/PushNotificationsViewModel.kt
@@ -0,0 +1,8 @@
+package com.yourssu.soomsil.usaint.screen.pushnotifications
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PushNotificationsViewModel @Inject constructor() : ViewModel()
