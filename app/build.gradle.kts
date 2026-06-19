f8e05b720ba776911832b8876e8127366d3a14cb
최종수 <chlwhdtn03@naver.com>
2026-06-19 13:20:42 +0900

    [release] 0.2.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.yourssu.soomsil.usaint"
         minSdk = 28
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.2.12"
+        versionCode = 26
+        versionName = "0.2.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
